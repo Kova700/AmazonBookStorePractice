4737442cda9ab0456134925f2a15266f9cb920e2
Tom <blak1002@gmail.com>
2023-12-19 03:21:52 +0900

    [config(app:build.gradle)] lifecycle-runtime-compose 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,6 +57,7 @@ dependencies {
 
 	implementation("androidx.core:core-ktx:1.12.0")
 	implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+	implementation ("androidx.lifecycle:lifecycle-runtime-compose:2.7.0-rc01")
 	implementation("androidx.activity:activity-compose:1.8.1")
 	implementation(platform("androidx.compose:compose-bom:2023.03.00"))
 	implementation("androidx.compose.ui:ui")
@@ -73,6 +74,7 @@ dependencies {
 	// Hilt
 	implementation("com.google.dagger:hilt-android:2.48.1")
 	ksp("com.google.dagger:hilt-android-compiler:2.48.1")
+	implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
 	// Jetpack-Navigation
 	implementation("androidx.navigation:navigation-compose:2.7.6")
 	// Kotlinx-serialization
