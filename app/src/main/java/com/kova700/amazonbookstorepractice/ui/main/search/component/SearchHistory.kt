ecce09b76292d162dfaa7cd943a667457e66dfa2
Tom <blak1002@gmail.com>
2024-01-24 22:25:53 +0900

    [refector] SearchHistoryItem Composable 함수로 따로 정의

--- app/src/main/java/com/kova700/amazonbookstorepractice/ui/main/search/component/SearchHistory.kt
+++ app/src/main/java/com/kova700/amazonbookstorepractice/ui/main/search/component/SearchHistory.kt
@@ -1,6 +1,5 @@
 package com.kova700.amazonbookstorepractice.ui.main.search.component
 
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -10,21 +9,15 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusManager
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.kova700.amazonbookstorepractice.R
 import kotlinx.collections.immutable.ImmutableList
 import kotlinx.collections.immutable.persistentListOf
 
@@ -34,7 +27,6 @@ fun SearchHistory(
 	onHistoryClick: (String) -> Unit,
 	focusManager: FocusManager? = null
 ) {
-
 	Column {
 
 		Spacer(modifier = Modifier.height(10.dp))
@@ -62,37 +54,16 @@ fun SearchHistory(
 
 		LazyColumn {
 			items(historyList) { historyString ->
-				Row(
-					modifier = Modifier
-						.fillMaxWidth()
-						.padding(horizontal = 20.dp),
-					horizontalArrangement = Arrangement.Absolute.SpaceBetween,
-					verticalAlignment = Alignment.CenterVertically,
-				) {
 
-					Text(
-						modifier = Modifier.clickable {
-							onHistoryClick(historyString)
-							focusManager?.clearFocus()
-						},
-						text = historyString,
-						fontSize = 14.sp
-					)
+				SearchHistoryItem(
+					historyString = historyString,
+					onHistoryClick = onHistoryClick,
+					focusManager = focusManager
+				)
 
-					IconButton(
-						onClick = {} //삭제 API
-					) {
-						Icon(
-							imageVector = ImageVector.vectorResource(id = R.drawable.ic_delete),
-							contentDescription = null,
-							tint = Color.Unspecified
-						)
-					}
-				}
 			}
 		}
 	}
-
 }
 
 @Preview(showBackground = true)
